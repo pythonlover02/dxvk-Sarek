28f619620f94ad1373390d52881326ca616d79b9
pythonlover02 <pythonloverv2@gmail.com>
2024-11-29 18:50:43 -0300

    [util] Backport config for Far Cry 2

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -984,6 +984,13 @@ namespace dxvk {
     { R"(\\SecretWorldLegends\.exe$)", {{
       { "d3d9.memoryTrackTest",              "True" },
     }} },
+    /* Far Cry 2: Set vendor ID to Nvidia to       *
+     * avoid vegetation artifacts on Intel, and    *
+     * set apitrace mode to True to improve perf   *
+     * on all hardware.                            */
+    { R"(\\(FarCry2|farcry2game)\.exe$)", {{
+      { "d3d9.customVendorId",              "10de" },
+      { "d3d9.apitraceMode",                "True" },
   }};
 
 
