9c77c356d817a1e12a9cb36c573c4cfda504b392
pythonlover02 <pythonloverv2@gmail.com>
2025-01-05 17:07:35 -0300

    release v1.10.6

--- src/dxvk/dxvk_instance.cpp
+++ src/dxvk/dxvk_instance.cpp
@@ -130,7 +130,7 @@ namespace dxvk {
     appInfo.pApplicationName      = appName.c_str();
     appInfo.applicationVersion    = 0;
     appInfo.pEngineName           = "DXVK";
-    appInfo.engineVersion         = VK_MAKE_VERSION(1, 10, 5);
+    appInfo.engineVersion         = VK_MAKE_VERSION(1, 10, 6);
     appInfo.apiVersion            = VK_MAKE_VERSION(1, 1, 0);
 
     VkInstanceCreateInfo info;
