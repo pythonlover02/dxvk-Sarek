670f4ab3f494a69464e20eaffd42809b9fc1a08e
pythonlover02 <pythonloverv2@gmail.com>
2025-01-08 17:00:35 -0300

    [dxvk] Always consider env var configuration as active
    Fixes this issue [https://github.com/doitsujin/dxvk/issues/4529]

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -1412,6 +1412,8 @@ namespace dxvk {
     }
 
     if (!confLine.empty()) {
+      ctx.active = true;
+
       // Inform the user that we parsing config from environment, might
       // help when debugging configuration issues
       Logger::info(str::format("Found config env: ", confLine));
